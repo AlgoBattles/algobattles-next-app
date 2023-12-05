186b97120d3d0ab01b756119fc1a79b0b1912022
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2023-12-04 23:38:43 -0500

    adds decline invite functionality

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -6,7 +6,9 @@ import { useInvites } from '../_contexts/InvitesContext';
 import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Button } from '@mui/material';
+import { Invite } from '../_types/inviteTypes'
 import Link from 'next/link'
+import { v4 as uuidv4 } from 'uuid';
 
 import { getUserInfo } from '../_helpers/userHelpers';
 
@@ -29,61 +31,98 @@ const Header = () => {
   const router = useRouter()
 
 const MailComponent = () => {
-
-
-      const [showJoinButton, setShowJoinButton] = useState(false);
-
       return (
         <div
+          key={uuidv4()}
           onMouseEnter={() => setShowMailComponent(true)}
           onMouseLeave={() => setShowMailComponent(false)}
+          className='absolute top-12 right-10 w-64 h-96' 
         >
-          <div className="absolute top-2/3 right-10 w-64 h-96 bg-gray-800 border border-gray-700 rounded-lg p-4">
+          <div style={{
+            width: 0,
+            height: 0,
+            marginLeft: '46%',
+            marginTop: '5px',
+            borderLeft: '15px solid transparent',
+            borderRight: '15px solid transparent',
+            borderBottom: '20px solid #2d3748' // color equivalent to gray-800 in Tailwind
+          }} />
+          <div className="w-64 h-96 bg-gray-800 border border-gray-700 rounded-lg p-4 overflow-y-scroll">
             {invites && invites.map((item) => (
-            <div
-            key={item.id + 1000}
-            className="flex flex-row items-center"
-            style={{ position: 'relative' }}
-            onMouseEnter={(e) => {
-                if ((e.target as HTMLElement).clientWidth / 2 < e.nativeEvent.offsetX) {
-                  setShowJoinButton(true);
-                }
-              }}
-              onMouseLeave={(e) => {
-                if ((e.target as HTMLElement).clientWidth / 2 > e.nativeEvent.offsetX) {
-                  setShowJoinButton(false);
-                }
-              }
-            }
-          >
-              <img src={item.sender && `/${item.senderAvatar}`} alt="avatar" className="w-10 h-10 rounded-full mr-2" />
-              <div className="flex flex-col">
-                <div className="font-bold">{item.sender && item.senderUsername}</div>
-                <div className="text-sm text-gray-500">invited you to a battle</div>
-              </div>
-              {showJoinButton && (
-                <Link href={`/home/waitingRoom/lobby?id=${item.id}`}>
-                <button
-                  style={{ position: 'absolute', top: '50%', right: 0, transform: 'translateY(-50%)' }}
-                >
-                  Join Lobby
-                </button>
-                </Link>
-              )}
-            </div>
+              <InviteComponent key={uuidv4()} item={item} />
             ))}
           </div>
         </div>
       );
     };
 
+const InviteComponent = ({item}: {item: Invite}) => {
+  const [showJoinButton, setShowJoinButton] = useState(false);
+  const [showDeclineButton, setShowDeclineButton] = useState(false);
+
+  return (
+    <div
+            key={uuidv4()}
+            className="flex flex-row items-center"
+            style={{ position: 'relative' }}
+          >
+              <img src={item.sender && `/${item.senderAvatar}`} alt="avatar" className="w-10 h-10 rounded-full mr-2" />
+              <div className="flex flex-col">
+                <div className="font-bold">{item.sender && item.senderUsername}</div>
+                <div className="text-sm text-gray-500">invited you to a battle</div>
+              </div>
+              <div 
+                onMouseEnter={() => setShowDeclineButton(true)}
+                onMouseLeave={() => setShowDeclineButton(false)}
+                className="absolute left-0 h-full w-1/2"
+                style={{ top: '50%', transform: 'translateY(-50%)' }}
+                >
+                {showDeclineButton && (
+                <Link href={`/home/waitingRoom/lobby?id=${item.id}`}>
+                <button
+                >
+                  Decline
+                </button>
+                </Link>
+              )}
+              </div>
+              <div 
+                onMouseEnter={() => setShowJoinButton(true)}
+                onMouseLeave={() => setShowJoinButton(false)}
+                className="absolute right-0 h-full w-1/2"
+                style={{ top: '50%', transform: 'translateY(-50%)' }}
+                >
+                {showJoinButton && (
+                <Link href={`/home/waitingRoom/lobby?id=${item.id}`}>
+                <button
+                >
+                  Join Lobby
+                </button>
+                </Link>
+              )}
+              </div>
+            </div>
+  )
+
+}
+
 const ProfileComponent = () => {
   return (
     <div
       onMouseEnter={() => setShowProfileComponent(true)}
       onMouseLeave={() => setShowProfileComponent(false)}
+      className='absolute top-12 right-0 w-64 h-96'
     >
-      <div className="absolute top-2/3 right-0 w-64 h-96 bg-gray-800 border border-gray-700 rounded-lg p-4">
+      <div style={{
+            width: 0,
+            height: 0,
+            marginLeft: '60%',
+            marginTop: '5px',
+            borderLeft: '15px solid transparent',
+            borderRight: '15px solid transparent',
+            borderBottom: '20px solid #2d3748' // color equivalent to gray-800 in Tailwind
+          }} />
+      <div className="w-64 h-96 bg-gray-800 border border-gray-700 rounded-lg p-4">
         {/* <div className="w-5 h-5 border-solid border-t-4 border-l-4 border-r-4 bg-red"></div> */}
         <Button onClick={handleSignOut} variant='outlined' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-[100%] rounded-3xl">
           Sign Out </Button>
---
+++ app/_types/inviteTypes.ts
@@ -0,0 +1,7 @@
+export interface Invite {
+    id: string;
+    sender: string;
+    senderUsername: string;
+    senderAvatar: string;
+    recipient: string;
+  }
