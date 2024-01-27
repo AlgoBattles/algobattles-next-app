56b97892842a11f4ede068cf932a508246a93d3f
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-01-27 15:31:25 -0500

    fixed bug where warning modal would reappear when logging in immediately after logging out

--- app/_components/WarningModal.tsx
+++ app/_components/WarningModal.tsx
@@ -2,20 +2,24 @@
 import React from 'react';
 import Link from 'next/link';
 import { useWarning } from '../_contexts/WarningContext';
+import { useRouter } from 'next/navigation'
 
 const WarningModal = (): React.JSX.Element | null => {
   const { info, setInfo } = useWarning()
+  const router = useRouter()
   if (!info.isOpen) {
     return null;
+  }
+  const navigateAndCloseModal = (linkToNavigate: string) => {
+    setInfo({ ...info, isOpen: false })
+    router.push(`${linkToNavigate}`)
   }
   return (
     <div className={`fixed inset-0 z-50 flex items-center justify-center ${info.isOpen ? '' : 'hidden'}`}>
       <div className="fixed inset-0 bg-black opacity-50"></div>
         <div className="relative h-[300px] w-[500px] bg-gray-700 p-4 rounded-lg text-center z-200 flex flex-col justify-between items-center py-8">
             <p>{info.warningMessage}</p>
-            <Link href={`${info.link}`}>
-                <button>{info.buttonTitle}</button>
-            </Link>
+                <button onClick={() => { navigateAndCloseModal(info.link) }}>{info.buttonTitle}</button>
             <button onClick={() => { setInfo({ ...info, isOpen: false }) }}>Return to previous page</button>
         </div>
       </div>
