ef3b3c0ba08f93268f163d83e81bcb702d3dd17e
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-01-29 13:07:06 -0500

    adds material UI email + password inputs

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,13 @@ import Link from 'next/link'
 import Head from 'next/head'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
+
 import EmailIcon from '@mui/icons-material/Email';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function Home (): React.ReactElement {
   const [selected, setSelected] = useState('signup')
@@ -14,6 +20,7 @@ export default function Home (): React.ReactElement {
   const [error, setError] = useState('')
   const router = useRouter()
   const supabase = createClientComponentClient()
+  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const handleEmailInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setEmail(event.target.value)
@@ -130,20 +137,99 @@ export default function Home (): React.ReactElement {
                         </button>
                       <button></button>
                     </div>
+                  <div className="flex items-center w-full justify-center mt-3 mb-6">
+                    <div className="bg-blue-500 border-t border-gray-600 flex-grow mr-3 ml-3"></div>
+                    <span className="text-gray-200 font-light text-sm">or</span>
+                    <div className="border-t border-gray-600 flex-grow ml-3 mr-3"></div>
+                  </div>
                   <div className="mb-6 mt-3">
-                    <label className="text-white text-sm mb-2 block font-medium">Email</label>
-                    <div className="flex items-center bg-gray-900 p-2 rounded">
-                      <input type="text" onChange={handleEmailInput} placeholder="admiralsnackbar@algobattles.xyz" className="bg-transparent text-white flex-grow focus:outline-none" />
-                    </div>
+                    <TextField
+                      onChange={handleEmailInput}
+                      id="outlined-basic"
+                      fullWidth variant="outlined"
+                      label='Email'
+                      sx={{
+                        '& .MuiOutlinedInput-root': {
+                          '& fieldset': {
+                            borderColor: '#2D3748',
+                          },
+                          '&:hover fieldset': {
+                            borderColor: '#4A5568',
+                          },
+                          '&.Mui-focused fieldset': {
+                            borderColor: 'primary.light',
+                          },
+                        },
+                        '& .MuiFormLabel-root': {
+                          color: '#6B7280', // Set the color of the label
+                        },
+                        '& .MuiFormLabel-root.Mui-focused': {
+                          color: 'primary.light', // Set the color of the label when focused
+                        },
+                        '& .MuiInputBase-input': { // Target the input text
+                          color: 'white', // Set the color of the input text
+                        },
+                        '& .MuiOutlinedInput-input': {
+                          '&::placeholder': {
+                            color: 'white',
+                            opacity: 1,
+                            textOverflow: 'ellipsis !important',
+                          },
+                        },
+                      }}></TextField>
                   </div>
-                  <div className="mb-6 mt-6">
-                    <label className="text-white text-sm mb-2 block font-medium">Password</label>
-                    <div className="flex items-center bg-gray-900 p-2 rounded">
-                      <input type="password" onChange={handlePasswordInput} placeholder="Password" className="bg-transparent text-white flex-grow focus:outline-none" />
-                    </div>
-                    <label className="text-red-500 text-sm mt-2 mb-2 block">{error}</label>
+                  <div className="mb-4 mt-6">
+                  <TextField
+                      type={showPassword ? 'text' : 'password'}
+                      onChange={handlePasswordInput}
+                      id="outlined-basic"
+                      fullWidth variant="outlined"
+                      label='Password'
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={() => { setShowPassword(!showPassword) }}
+                              edge="end"
+                            >
+                              {showPassword ? <VisibilityOff sx={{ color: '#4A5568' }}/> : <Visibility sx={{ color: '#4A5568' }}/>}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                      sx={{
+                        '& .MuiOutlinedInput-root': {
+                          '& fieldset': {
+                            borderColor: '#2D3748',
+                          },
+                          '&:hover fieldset': {
+                            borderColor: '#4A5568',
+                          },
+                          '&.Mui-focused fieldset': {
+                            borderColor: 'primary.light',
+                          },
+                        },
+                        '& .MuiFormLabel-root': {
+                          color: '#6B7280', // Set the color of the label
+                        },
+                        '& .MuiFormLabel-root.Mui-focused': {
+                          color: 'primary.light', // Set the color of the label when focused
+                        },
+                        '& .MuiInputBase-input': { // Target the input text
+                          color: 'white', // Set the color of the input text
+                        },
+                        '& .MuiOutlinedInput-input': {
+                          '&::placeholder': {
+                            color: 'white',
+                            opacity: 1,
+                            textOverflow: 'ellipsis !important',
+                          },
+                        },
+                      }}></TextField>
+                    <label className="text-red-500 w-full h-[20px] text-sm mt-2 mb-2 block">{error}</label>
                   </div>
-                    <button onClick={() => { handleSignIn().catch(console.error) }} className="bg-orange-500 text-white w-full py-2 font-medium rounded-3xl">Sign In</button>
+                    <button onClick={() => { handleSignIn().catch(console.error) }} className="bg-blue-600 hover:bg-blue-700 text-white w-full py-2.5 font-medium rounded-3xl">Sign In</button>
                 </div>
             )}
         </div>
