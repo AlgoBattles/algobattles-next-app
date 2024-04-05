b5b932b8015aee2dc585ee3f066d1b0e341535af
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-04-05 12:56:33 -0400

    adds styling to warning modal

--- app/_components/WarningModal.tsx
+++ app/_components/WarningModal.tsx
@@ -19,22 +19,26 @@ const WarningModal = (): React.JSX.Element | null => {
       className={`fixed inset-0 z-50 flex items-center justify-center ${info.isOpen ? "" : "hidden"}`}
     >
       <div className="fixed inset-0 bg-black opacity-50"></div>
-      <div className="relative h-[300px] w-[500px] bg-gray-700 p-4 rounded-lg text-center z-200 flex flex-col justify-between items-center py-8">
-        <p>{info.warningMessage}</p>
-        <button
-          onClick={() => {
-            navigateAndCloseModal(info.link);
-          }}
-        >
-          {info.buttonTitle}
-        </button>
-        <button
-          onClick={() => {
-            setInfo({ ...info, isOpen: false });
-          }}
-        >
-          Return to previous page
-        </button>
+      <div className="relative h-[300px] w-[500px] bg-gray-900 border-[1px] border-gray-700 p-4 rounded-lg text-center z-200 flex flex-col justify-between items-center py-8">
+        <p className="text-lg font-medium">{info.warningMessage}</p>
+        <div className="flex flex-row">
+          <button
+            className="px-6 py-2 mr-4 font-medium bg-red-500 hover:bg-red-600 rounded-3xl"
+            onClick={() => {
+              navigateAndCloseModal(info.link);
+            }}
+          >
+            {info.buttonTitle}
+          </button>
+          <button
+            className="px-6 py-2 ml-4 font-medium bg-blue-500 hover:bg-blue-600 rounded-3xl"
+            onClick={() => {
+              setInfo({ ...info, isOpen: false });
+            }}
+          >
+            Return to previous page
+          </button>
+        </div>
       </div>
     </div>
   );
